0ca13cb420cb1bbd85dd384d578eb8a2b35a3a8e
Farouk Francis <46872062+importfrankcs@users.noreply.github.com>
2019-09-25 15:51:35 -0700

    ddnt change muuch, this is todays code

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 //import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:google_sign_in/google_sign_in.dart';
+// this is also very useless text for test purpsoes
 
 void main() => runApp(MyApp());
 
