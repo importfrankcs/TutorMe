277bee9ae246fa32b0a48bfd61fbcbe7668ca727
ibraaheem <ibraaheem14@gmail.com>
2019-10-23 16:48:00 +0200

    Ibraaheem / Consultations

--- lib/studentPages/consultation.dart
+++ lib/studentPages/consultation.dart
@@ -259,6 +259,104 @@ class _getConsultationsState extends State<getConsultations> {
   }
 }
 
+class getCompleted extends StatefulWidget {
+  final List<DocumentSnapshot> documents;
+  getCompleted({this.documents});
+
+  @override
+  _getCompletedState createState() => _getCompletedState();
+}
+
+class _getCompletedState extends State<getCompleted> {
+  final ratingcomment = TextEditingController();
+  var rating = 0.0;
+  final studbioController = TextEditingController();
+
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.documents.length,
+        itemExtent: 110.0,
+        itemBuilder: (BuildContext context, int index) {
+          String toStudent = widget.documents[index].data['Student'].toString();
+          String day = widget.documents[index].data['Day'].toString();
+          String modu = widget.documents[index].data['Module'].toString();
+          String time = widget.documents[index].data['Time'].toString();
+          String ven = widget.documents[index].data['Venue'].toString();
+          String tut = widget.documents[index].data['Tutor'].toString();
+          String userid = widget.documents[index].data['uid'].toString();
+          String title =
+          ('${toStudent[0].toUpperCase()} ${toStudent.split(" ").last[0].toUpperCase()}${toStudent.split(" ").last.toString().substring(1).toLowerCase()}');
+          String daytitle =
+          ('${day[0].toUpperCase()} ${day.split(" ").last[0].toUpperCase()}${day.split(" ").last.toString().substring(1).toLowerCase()}');
+          String module =
+          ('${modu[0].toUpperCase()} ${modu.split(" ").last[0].toUpperCase()}${modu.split(" ").last.toString().substring(1).toLowerCase()}');
+
+          String times =
+          ('${time[0].toUpperCase()} ${time.split(" ").last[0].toUpperCase()}${time.split(" ").last.toString().substring(1).toLowerCase()}');
+
+          String venue =
+          ('${ven[0].toUpperCase()} ${ven.split(" ").last[0].toUpperCase()}${ven.split(" ").last.toString().substring(1).toLowerCase()}');
+
+          String tutor =
+          ('${tut[0].toUpperCase()} ${tut.split(" ").last[0].toUpperCase()}${tut.split(" ").last.toString().substring(1).toLowerCase()}');
+//documents[index].data['displayName'].toString()
+          return ListTile(
+            //leading: Text(documents[index].data['displayName'].toString()),
+            //leading: ,
+            title: Row(children: <Widget>[
+              Text("Module: ", style: TextStyle(fontWeight: FontWeight.w500)),
+              Text(modu),
+            ]),
+            subtitle: Row(children: <Widget>[
+              Text("Tutor: ", style: TextStyle(fontWeight: FontWeight.w800)),
+              Text(tut),
+            ]),
+
+            isThreeLine: true,
+            trailing: IconButton(
+              icon: Icon(Icons.rate_review),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    // {Please work
+                    builder: (BuildContext context) {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            AlertDialog(
+                              title: new Text('Request From:\n$toStudent'),
+                              content: Text(
+                                  'Module: $modu\nDay: $day\nTime: $time\nVen: $ven\nDetails: \n$com'),
+                              actions: <Widget>[
+                                FlatButton(
+                                  child:
+
+                                FlatButton(
+                                  child: new Text(
+                                    'CLOSE',
+                                    style:
+                                    TextStyle(color: Colors.blue),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                )],
+                            )
+
+                          ],
+                        ),
+                      );
+                    });
+              },
+            ),
+            //trailing: , Place Rating here
+          );
+        });
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -421,32 +519,26 @@ class PageTwo extends StatelessWidget {
 
 class PageThree extends StatelessWidget {
   Widget build(BuildContext context) {
-    return new StreamBuilder(
-      stream: Firestore.instance
-          .collection("Consultations")
-          .where("Student", isEqualTo: usern.username.toString())
-          .snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData)
-          return Center(child: CircularProgressIndicator());
-        return Card(
-          child: new ListView(
-            children: snapshot.data.documents.map((document) {
-              return new ListTile(
-                  title: Text(document["Student"],
-                      style: new TextStyle(
-                          fontWeight: FontWeight.w400, fontSize: 18.0)),
-                  leading: new Icon(
-                    Icons.check_circle,
-                    color: Colors.blue[600],
-                  ),
-                  //------------------------------------------------------->
-                  onTap: () {});
-            }).toList(),
-          ),
-        );
-      },
+    //Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('CompletedSessions')
+            .where("Student",
+            isEqualTo:
+            "${usern.username}") //button name, enable dynamic var
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.blueAccent),
+                ));
+          return getCompleted(documents: snapshot.data.documents);
+        },
+      ),
     );
+
   }
 
   /*
